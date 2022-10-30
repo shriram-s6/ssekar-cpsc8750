265eec57a6ce212787affe9df5272aa820614834
shriram-s6 <shriram.sekar94@gmail.com>
2022-10-30 16:34:17 -0400

    Trivia Questions

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 // use the express library
 const express = require('express');
+const {encode} = require('html-entities');
 const cookieParser = require('cookie-parser');
 // create a new server application
 const app = express();
-
+const fetch = require('node-fetch');
 // Define the port we will listen on
 // (it will attempt to read an environment global
 // first, that is for when this is used on the real
@@ -48,6 +49,43 @@ app.get('/', (req, res) => {
         message: message
     });
 });
+
+app.get('/trivia', async (req, res) => {
+    // fetch the data
+    const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
+
+    if (!response.ok) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+        return;
+    }
+
+    const htmlContent = await response.json();
+
+    const htmlFormat = JSON.stringify(htmlContent, 2);
+
+    const correctAns = htmlContent.results[0].correct_answer;
+    const answers = htmlContent.results[0].incorrect_answers;
+    answers.push(correctAns);
+
+    answers.sort(() => Math.random() - 0.5);
+
+    const linksToAnswers = answers.map(answer => {
+        return `<a href="javascript:alert('${
+        answer === correctAns ? 'Correct!' : 'Incorrect, Please Try Again!'
+        }')">${answer}</a>`
+    });
+
+    res.render(
+        'trivia', {
+            category: htmlContent.results[0].category,
+            difficulty: htmlContent.results[0].difficulty,
+            question: htmlContent.results[0].question,
+            answers: answers,
+            linksToAnswers: linksToAnswers,
+        }
+    );
+})
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
